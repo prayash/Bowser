fac1a68b2684c9f0ef0261e12f761e963efc556d
Prayash Thapa <p@prayash.io>
2025-03-09 11:56:53 -0700

    Initial Commit

---
+++ Bowser/BowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowserApp.swift
+//  Bowser
+//
+//  Created by Prayash Thapa on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct BowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bowser
+//
+//  Created by Prayash Thapa on 3/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
